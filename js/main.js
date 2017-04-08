0fb5dbeeae8f354d029b3093d30624ec7424fd60
Andrei-Zhuk <zhuk_andrey@tut.by>
2017-04-08 20:55:03 +0300

    new try

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.spritesheet('dude', 'assets/spritesheet.png', 80, 89);
